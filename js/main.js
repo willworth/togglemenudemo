9cccec4de61a04b2c1081d4113cab1cb52c35052
Will Worth <will.worth@gmail.com>
2019-04-23 19:14:05 +0200

    menu style

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
- // Look for .hamburger
+const menu = document.querySelector('.menu');
+    
+ //  hamburger animation //   
+    
+    // Look for .hamburger
     var hamburger = document.querySelector(".hamburger");
     // On click
     hamburger.addEventListener("click", function() {
       // Toggle class "is-active"
       hamburger.classList.toggle("is-active");
+      menu.classList.toggle("is-open");
       // Do something else, like open/close menu
+      
     });
+
+    
+
+
+
+
+
+
+
+//  //  hamburger animation //   
+    
+//     // Look for .hamburger
+//     var hamburger = document.querySelector(".hamburger");
+//     // On click
+//     hamburger.addEventListener("click", function() {
+//       // Toggle class "is-active"
+//       hamburger.classList.toggle("is-active");
+//       // Do something else, like open/close menu
+//     });
---
+++ readme.md
@@ -0,0 +1,5 @@
+# toggle demo
+
+This is a toy project working on navigation animations.
+
+See the [ hamburger project ](https://github.com/jonsuh/hamburgers)
